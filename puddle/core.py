37d153e35dc19c97621355c2f6673c63125f5c7b
Colin Maudry <colin@maudry.com>
2022-06-15 19:45:44 +0200

    💡 create_dataset function skeleton

---
+++ puddle/core.py
@@ -0,0 +1,19 @@
+def create_dataset(urls=[str], dataset_series_id=str):
+
+    # download the files
+
+    # analyze the files: file names, file size check sums, number of lines, columns, profiling
+
+    # create a new dataset with the provided info
+
+    # create the distributions and link the dataset
+
+    # create a new data series if needed and link the dataset
+
+    # push the files to S3
+
+    # add the dataset graph to the main graph store
+
+    # save the graph store
+    pass
+
